6f41327b4b7ae1d3d30cf7d5117bdd88a16ee4bc
Anmol Sharma <anmol@appzoy.com>
2018-10-31 18:54:52 +0530

    react-native-navigation v-2 working model with complete configurations.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Navigation } from "react-native-navigation";
+import App from "./App";
+Navigation.registerComponent(`com.codigo.FirstScreen`, () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
